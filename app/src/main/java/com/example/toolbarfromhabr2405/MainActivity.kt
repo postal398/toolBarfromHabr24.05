1852458b804826addc74b2caff49fd3955d2cb00
Vadim <maslovvadim398@gmail.com>
2024-05-24 16:47:47 +0300

    Just start

---
+++ app/src/main/java/com/example/toolbarfromhabr2405/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.toolbarfromhabr2405
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.toolbarfromhabr2405.ui.theme.ToolBarfromHabr2405Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ToolBarfromHabr2405Theme {
+                customToolBar()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ToolBarfromHabr2405Theme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/toolbarfromhabr2405/customToolBar.kt
@@ -0,0 +1,10 @@
+package com.example.toolbarfromhabr2405
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.MeasurePolicy
+
+@Composable
+fun customToolBar() {
+    Layout()
+}
